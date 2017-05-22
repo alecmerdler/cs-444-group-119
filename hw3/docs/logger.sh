efdcea237666de7dced78cf77315d4a591dfed53
Arthur Shing <arthur.shing@gmail.com>
2017-05-22 15:06:27 -0700

    Added hw3 docs

---
+++ hw3/docs/logger.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+
+origin=`git config remote.origin.url`
+base=`dirname "$origin"`/`basename "$origin" .git`
+
+# Output LaTeX table in chronological order
+echo "\\begin{tabular}{l l l}\\textbf{Detail} & \\textbf{Author} & \\textbf{Description} & \\textbf{Date}\\\\\\hline"
+git log --pretty=format:"\\href{$base/commit/%H}{%h} & %an & %s & %ad\\\\\\hline"
+echo "\end{tabular}"
