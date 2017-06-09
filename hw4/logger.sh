127e114c2a48cf4896cfb1b315297ece28c49e9a
Arthur Shing <arthur.shing@gmail.com>
2017-06-09 12:42:27 -0700

    Changes to doc and vclog script

--- hw4/logger.sh
+++ hw4/logger.sh
@@ -6,5 +6,5 @@ base=`dirname "$origin"`/`basename "$origin" .git`
 
 # Output LaTeX table in chronological order
 echo "\\begin{tabular}{l l l l}\\textbf{Detail} & \\textbf{Author} & \\textbf{Description} & \\textbf{Date}\\\\\\hline"
-git log --pretty=format:"\\href{$base/commit/%H}{%h} & %an & %s & %ad\\\\\\hline" --after="2017-05-15"
-echo "\end{tabular}"
+git log --pretty=format:"\\href{$base/commit/%H}{%h} & %an & %s & %ad\\\\\\hline" --after="2017-05-23"
+echo "\end{tabular}" 
